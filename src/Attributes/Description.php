5f8a383cf31402bc0d54edbb073922b32224707a
Jeramy Hing <jeramy.hing@gmail.com>
2024-11-21 20:30:51 -0500

    feat(attributes): explicitly define attribute target restrictions
    - Add deprecation notices for class-level Id and Label attributes
    - Document recommended usage patterns in PHPDoc
    - Plan removal of class-level support in v2.0.0

--- src/Attributes/Description.php
+++ src/Attributes/Description.php
@@ -4,9 +4,18 @@ namespace Iteks\Attributes;
 
 use Attribute;
 
-#[Attribute]
+/**
+ * Provides descriptive text for enum classes and cases.
+ *
+ * This attribute can be used on both class and case level, and supports multiple instances
+ * to provide different types of descriptions (e.g., short/long form, different languages).
+ */
+#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_CLASS_CONSTANT | Attribute::IS_REPEATABLE)]
 class Description
 {
+    /**
+     * @param  string  $description
+     */
     public function __construct(
         public string $description,
     ) {
--- src/Attributes/Label.php
+++ src/Attributes/Label.php
@@ -4,9 +4,18 @@ namespace Iteks\Attributes;
 
 use Attribute;
 
-#[Attribute]
+/**
+ * Provides a display label for enum cases.
+ *
+ * @deprecated 1.1.0 Using this attribute on classes is deprecated and will be removed in v2.0.0.
+ *             It should only be used on enum cases.
+ */
+#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_CLASS_CONSTANT | Attribute::IS_REPEATABLE)]
 class Label
 {
+    /**
+     * @param  string  $label
+     */
     public function __construct(
         public string $label,
     ) {
